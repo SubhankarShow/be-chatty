a9d89c5970ad668e2393983f004cddbcf20eb8da
Subhankar <showduggu1@gmail.com>
2020-08-22 01:10:10 +0530

    peer server change

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-  host: "/",
-  port: "process.env.PORT || 3001",
+  secure: true,
+  host: "bechatty.herokuapp.com",
+  port: 443,
 });
 const myVideo = document.createElement("video");
 myVideo.muted = true;
